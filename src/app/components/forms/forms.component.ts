8093bee5b3121f3c7cbfe471453a630fd8a560ad
Nelson Feijó <114948173+nelsonfeijo@users.noreply.github.com>
2024-08-19 14:19:04 -0300

    Sevice

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { GetUnitsService } from '../../services/get-units.service';
 
 @Component({
   selector: 'app-forms',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './forms.component.html',
   styleUrl: './forms.component.scss'
 })
-export class FormsComponent {
-  results = []
+
+export class FormsComponent implements OnInit {
+  results = [];
+  formGroup!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private unitService: GetUnitsService) {
+
+  };
+  ngOnInit():void {
+    this.unitService.getAllUnits().subscribe(data => console.log(data))
+    this.formGroup = this.formBuilder.group({
+      hour: '',
+      showClosed: false,
+
+    })
+  }
+
+  onSubmit(): void{
+    console.log(this.formGroup.value)
+  }
+  onClean(): void{
+    this.formGroup.reset()
+  }
+
 }
